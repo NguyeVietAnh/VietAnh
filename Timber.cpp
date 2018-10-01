e3edd456ca6509bc5f6e7f18c48648c4a6acaa93
nguyen viet anh <vietanh12g@gmail.com>
2018-10-01 08:55:57 +0700

    ban thu 2

--- Timber.cpp
+++ Timber.cpp
@@ -7,20 +7,7 @@ using namespace sf;
 
 int main()
 {
-	// Create a video mode object
-	VideoMode vm(1920, 1080);
 
-	// Create and open a window for the game
-	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
-
-	// Create a texture to hold a graphic on the GPU
-	Texture textureBackground;
-
-	// Load a graphic into the texture
-	textureBackground.loadFromFile("graphics/background.png");
-
-	// Create a sprite
-	Sprite spriteBackground;
 
 	// Attach the texture to the sprite
 	spriteBackground.setTexture(textureBackground);
